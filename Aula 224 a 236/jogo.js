cf113e42b781626444ac95cbea7e764816ce876d
Italo <italogabrielvc@gmail.com>
2021-11-03 17:07:48 -0300

    App game mata mosquito, Tamanhos randômicos

--- Aula 224 a 236/jogo.js
+++ Aula 224 a 236/jogo.js
@@ -9,26 +9,43 @@ function ajustaTamanhoPalcoJogo() {
 	console.log(largura, altura)
 }
 
-ajustaTamanhoPalcoJogo()
+	ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
-var posicaoX = Math.floor(Math.random() * largura) - 90
-var posicaoY = Math.floor(Math.random() * altura) - 90 
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90 
 
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-posicaoY = posicaoY < 0 ? 0 : posicaoY
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-console.log(posicaoX, posicaoY)
+	console.log(posicaoX, posicaoY)
 
-//criar o elemento html
-var mosquito = document.createElement('img')
-mosquito.src = 'imagens/mosca.png'
-mosquito.className = 'mosquito1'
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
+	//criar o elemento html
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosca.png'
+	mosquito.className = tamanhoAleatorio()
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
 
 
-document.body.appendChild(mosquito)
+	document.body.appendChild(mosquito)
+
+}
+
+function tamanhoAleatorio() {
+	var classe =Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+
+		case 1:
+			return 'mosquito2'
+
+		case 2: 
+			return 'mosquito3'
+
+	}
 }
 
